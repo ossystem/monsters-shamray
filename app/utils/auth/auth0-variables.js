edd0387c4d9e6cee9f673d7faca66ac14b7d59d0
okeanrst <okeanrst@gmail.com>
2019-05-14 21:41:45 +0300

    return dynamic import to app, change callback url

--- app/utils/auth/auth0-variables.js
+++ app/utils/auth/auth0-variables.js
@@ -3,7 +3,7 @@
 export const AUTH_CONFIG = {
   domain: 'dev-cozd59bg.eu.auth0.com',
   clientId: 'sCvsfVJWzmog9KsSJ4CoWQUxVTDL_obY',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://shamray-monsters.ossystem.ua/callback',
   realm: 'Username-Password-Authentication',
   audience: 'https://dev-cozd59bg.eu.auth0.com/api/v2/',
   issuer: 'https://dev-cozd59bg.eu.auth0.com',
